c77b0d7bd649c63c86b5e60fe2e0d2c03604711f
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-25 20:47:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,6 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-<<<<<<< HEAD
     case @operation 
       when "add"
       "#{@number1 + @number2}"
@@ -45,10 +44,8 @@ class App < Sinatra::Base
     end
   end
 
-=======
     erb :operation
   end
 
 
->>>>>>> e175be60b6affa51d265fe0dfebea60e5ac2138b
 end
